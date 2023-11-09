3369c00db63e071ed3a4e095ff17f3a2893a96dd
Martin S <martingsoto47@gmail.com>
2023-11-09 01:09:13 -0300

    agregado de detalles en modelos

--- control_post/models.py
+++ control_post/models.py
@@ -11,6 +11,9 @@ class usuario(models.Model):
     email = models.EmailField(blank=True)
     
 
+    def __str__ (self):
+        return f"{self.nombre} , {self.apellido}, {self.dni} "
+
 
 
 class genero(models.Model):
@@ -18,12 +21,18 @@ class genero(models.Model):
     cantidad = models.IntegerField()
 
 
+    def __str__ (self):
+        return f"{self.nombre} , {self.cantidad}"
+
 class blogpost(models.Model):
     autor = models.CharField(max_length=64)
     gen = models.CharField(max_length=64)
     fecha = models.DateField(null=False, blank=False)
     titulo = models.CharField(max_length=126)
     content = models.TextField(blank=False)
+
+    def __str__ (self):
+        return f"{self.autor} , {self.gen}, {self.fecha}, {self.titulo}"
 
 
 
